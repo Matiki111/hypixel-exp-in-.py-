5c226b7c7781ebbd962d74a225415ae1a8d88f04
Matiki111 <matiser14@gmail.com>
2021-11-29 19:02:39 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+
+poziom = int(input("Jaki poziom hypixela cię interesuje?: "))
+
+mnoznik2 = poziom - 2
+rel = 2500 * mnoznik2
+
+
+if poziom == 1:
+    print("Exp, który będziesz potrzebował, aby osiągnąć ten poziom to: 0")
+else:
+ print("Exp, który będziesz potrzebował, aby osiągnąć ten poziom to: " + str(10000 + rel))
+
+ input()
